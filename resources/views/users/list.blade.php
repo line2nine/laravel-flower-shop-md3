bf5bb3bfff5a36fd43a6ab2eb1d10bdb8ab8894c
line2nine <hailinh203@gmail.com>
2020-06-10 16:17:10 +0700

    List user

--- resources/views/users/list.blade.php
+++ resources/views/users/list.blade.php
@@ -1,1 +1,71 @@
-<?php
+@extends('admin.master')
+@section('content')
+    <div class="row pt-2 pb-2">
+        <div class="col-sm-9">
+            <h4 class="page-title">Simple Tables</h4>
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="javaScript:void();">Dashtreme</a></li>
+                <li class="breadcrumb-item"><a href="javaScript:void();">Tables</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Simple Tables</li>
+            </ol>
+        </div>
+        <div class="col-sm-3">
+            <div class="btn-group float-sm-right">
+                <button type="button" class="btn btn-light waves-effect waves-light"><i class="fa fa-cog mr-1"></i> Setting</button>
+                <button type="button" class="btn btn-light dropdown-toggle dropdown-toggle-split waves-effect waves-light" data-toggle="dropdown">
+                    <span class="caret"></span>
+                </button>
+                <div class="dropdown-menu">
+                    <a href="javaScript:void();" class="dropdown-item">Action</a>
+                    <a href="javaScript:void();" class="dropdown-item">Another action</a>
+                    <a href="javaScript:void();" class="dropdown-item">Something else here</a>
+                    <div class="dropdown-divider"></div>
+                    <a href="javaScript:void();" class="dropdown-item">Separated link</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Users List</h5>
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Image</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($users as $key => $user)
+                                <tr>
+                                    <th scope="row">{{++$key}}</th>
+                                    <td><img src="{{asset('storage/' . $user->image)}}" class="avatar"></td>
+                                    <td>{{$user->name}}</td>
+                                    <td>{{$user->email}}</td>
+                                    <td>
+                                        <a class="btn btn-primary btn-round px-2" href="#"><i class="icon-size-fullscreen"></i>
+                                            Profile
+                                        </a>
+                                        <a class="btn btn-warning btn-round px-2" href="#"><i class="icon-settings"></i>
+                                            Edit
+                                        </a>
+                                        <a class="btn btn-danger btn-round px-2" href="#"><i class="icon-trash"></i>
+                                            Delete
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
